1aae30a0d5e63de8981a7d5fc9a97679f6e90154
Abdoulayedeveloppeur <an403937@gmail.com>
2026-07-07 16:08:41 +0000

    first commit

---
+++ src/main/java/sn/edu/sn/isepat/tic/dbe/p6/gestion_commandes_springboot/GestionCommandesSpringbootApplication.java
@@ -0,0 +1,13 @@
+package sn.edu.sn.isepat.tic.dbe.p6.gestion_commandes_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionCommandesSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionCommandesSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sn/edu/sn/isepat/tic/dbe/p6/gestion_commandes_springboot/GestionCommandesSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package sn.edu.sn.isepat.tic.dbe.p6.gestion_commandes_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionCommandesSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
